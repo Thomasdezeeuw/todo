cc8ef663ad7d33daf23d335489568426c5e86be3
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-02-01 22:30:37 +0100

    Add quotes to all variables

--- todo.sh
+++ todo.sh
@@ -15,9 +15,9 @@ EDTIOR=${EDITOR:-vim}
 
 # Create the git repo, if needed.
 setup() {
-	mkdir -p $TODO_ROOT
+	mkdir -p "$TODO_ROOT"
 	if [[ ! -f "$TODO_ROOT/.git/HEAD" ]]; then
-		git init --quiet $TODO_ROOT
+		git init --quiet "$TODO_ROOT"
 	fi
 }
 
@@ -38,34 +38,34 @@ open() {
 	# Setup a skeleton if the file doesn't exists.
 	if [[ ! -f "$LIST_FILE_FULL_PATH" ]]; then
 		if [ -z ${1+x} ]; then
-			printf "# TODO \n\n\n" > $LIST_FILE_FULL_PATH
+			printf "# TODO \n\n\n" > "$LIST_FILE_FULL_PATH"
 		else
-			printf "# TODO $1\n\n\n" > $LIST_FILE_FULL_PATH
+			printf "# TODO $1\n\n\n" > "$LIST_FILE_FULL_PATH"
 		fi
-		(cd $TODO_ROOT && git add $LIST_FILE_PATH)
+		(cd "$TODO_ROOT" && git add "$LIST_FILE_PATH")
 	fi
 
 	# Open the users editor to allow them to edit the todo file.
-	$EDITOR $LIST_FILE_FULL_PATH
+	$EDITOR "$LIST_FILE_FULL_PATH"
 
 	# If the file is empty we can remove it.
-	if [[ ! -s $LIST_FILE_FULL_PATH ]]; then
-		rm -f $LIST_FILE_FULL_PATH
+	if [[ ! -s "$LIST_FILE_FULL_PATH" ]]; then
+		rm -f "$LIST_FILE_FULL_PATH"
 	fi
 
 	# Commit the changes.
 	# TODO: improve commit message
-	(cd $TODO_ROOT && git commit --quiet --all --message="Update todo list" || true)
+	(cd "$TODO_ROOT" && git commit --quiet --all --message="Update todo list" || true)
 
 	# Automatically sync the todo, if that is required.
-	if [[ $TODO_AUTO_SYNC == "true" || $TODO_AUTO_SYNC == "1" ]]; then
+	if [[ "$TODO_AUTO_SYNC" == "true" || "$TODO_AUTO_SYNC" == "1" ]]; then
 		sync
 	fi
 }
 
 # Call git push on the todo repo.
 sync() {
-	(cd $TODO_ROOT && git push --quiet)
+	(cd "$TODO_ROOT" && git push --quiet)
 }
 
 setup
